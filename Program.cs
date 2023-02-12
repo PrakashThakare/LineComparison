5c5b78f3f4d9817cad0c44f34a80629704d7b696
Shree Thakre <shreet365@gmail.com>
2023-02-12 06:23:57 +0530

    Calculating Length

--- Program.cs
+++ Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("WelCome to Line Comparison");
+            Console.Write("Enter Starting Point: ");
+            Double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Ending Point: ");
+            Double x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Starting Point: ");
+            Double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Ending Point: ");
+            Double y2 = Convert.ToDouble(Console.ReadLine());
+
+            Double Length = Math.Sqrt(Math.Pow((x2 - x1), 2) + (Math.Pow((y2 - y1), 2)));
+            Console.WriteLine("Length is : " + Length);
+
+            
             Console.ReadLine();
         }
     }
