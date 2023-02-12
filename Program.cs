220853c2ee3162fda5746dd1d673e57670d9a065
Shree Thakre <shreet365@gmail.com>
2023-02-12 06:08:53 +0530

    Demo

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome to Line Comparison");
+            Console.ReadLine();
+        }
+    }
+}
